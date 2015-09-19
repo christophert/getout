13c6f8d51c73f3c69a6541dc0a91cd50893f1f70
starman360 <anmol.modur@gmail.com>
2015-09-19 19:45:15 -0400

    woot

--- public/js/jq.js
+++ public/js/jq.js
@@ -2,10 +2,10 @@ jQuery(document).ready(function() {
     jQuery('#trigger').on('click', function(e)  {
  
         // Show/Hide Tabs
-        jQuery('#sub').fadeOut(400);
+        jQuery('#sub').fadeOut(2000);
         jQuery('#sub').addClass('hidden');
         jQuery('form:first-of-type').removeClass('hidden');
-		jQuery('form:first-of-type').delay(1200).fadeIn(1200);
+		jQuery('form:first-of-type').delay(400).fadeIn(1200);
         e.preventDefault();
     });
 });
