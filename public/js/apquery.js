1eba33642c90887213cc21f91db8eafae3566b3f
officer <me@christran.in>
2015-09-20 04:53:18 -0400

    asdfafd

--- public/js/apquery.js
+++ public/js/apquery.js
@@ -5,7 +5,7 @@ jQuery("#tag_holder div:nth-of-type(1)").ready(function() {
 	jQuery.ajax({
 		type: "GET",
 		url: "http://aggregate.gogogogo.co/yolo/yolo/5/rochester/pittsburgh",
-		timeout: 2000,
+		timeout: 10000,
 		success: function(r) {
 			console.log(r);
 			// jQuery("p#flightId").html()
