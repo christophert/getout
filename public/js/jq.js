907743b44f5c79fd3975fbaf4d04ee83973d7c88
starman360 <anmol.modur@gmail.com>
2015-09-19 21:01:28 -0400

    fixed mapping of second button

--- public/js/jq.js
+++ public/js/jq.js
@@ -10,7 +10,7 @@ jQuery(document).ready(function() {
         e.preventDefault();
     });
     
-    $("button").click(function(){
+    $("#next").click(function(){
         $("html").load("");
     });
     
