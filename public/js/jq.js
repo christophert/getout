b0a163458a090137372c0fd178f329b293c000a8
starman360 <anmol.modur@gmail.com>
2015-09-20 07:12:20 -0400

    things

--- public/js/jq.js
+++ public/js/jq.js
@@ -1,9 +1,5 @@
 jQuery(document).ready(function() {
-    $("#trigger").click(function(e){
-        e.preventDefault();
-        var location = document.getElementsByName('input')[0].value;
-        var numOfDays = document.getElementsByName('input')[1].value;
-        $("#trigger").attr("onclick","location.herf='/plan?days=' +  numOfDays + '&to=' + location");
-    });
-    
+    var location = document.getElementsByName('input')[0].value;
+    var numOfDays = document.getElementsByName('input')[1].value;
+    $("#trigger").click().attr("onclick","location.herf='/plan?days=' +  numOfDays + '&to=' + location");
 });
