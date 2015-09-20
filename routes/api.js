ca344fc327d6fcfb2dc4a1c4b1bbf0fd28b724c7
officer <me@christran.in>
2015-09-20 04:27:53 -0400

    i broked

--- routes/api.js
+++ routes/api.js
@@ -124,69 +124,6 @@ router.get('/places/:loc/:query', function(req, res, next) {
 
 });
 
-router.get('/yolo/:howlong/:from/:to', function(req, res, next) {
-	var howlong = req.params.howlong;
-	var now = moment();
-	var later = moment().add(howlong, 'days');
-	now = now.format("YYYYMMDD");
-	later = later.format("YYYYMMDD");
-	var whereFrom = req.params.from;
-	var whereTo = req.params.to;
-	
-	//query for airport code
-	function _toAirportCode(originLoc, _callback) {
-		request('http://www.priceline.com/svcs/ac/index/flights/'+originLoc,function(error, response, body) {
-			if(!error && response.statusCode == 200) {
-				var airportsRaw = JSON.parse(body)["searchItems"];
-				var airports = _.map(airportsRaw, function(airport) {
-					return {
-						'id': airport.id,
-						'name': airport.itemName,
-						'score': airport.score
-					};
-				});
-				// console.log(airports);
-				// console.log(_.max(airports, 'score').id);
-				if(typeof(_callback) == "function") {
-					_callback(_.max(airports, 'score').id);
-				}
-			}
-		});
-	}
-
-	var fromAirportCode, toAirportCode;
-	_toAirportCode(whereFrom, function(id) {
-		fromAirportCode = id;
-		_toAirportCode(whereTo, function(id) {
-			toAirportCode = id;
-			
-			
-			request('/api/flights/'+fromAirportCode+'/'+toAirportCode+'/'+now+'/'+later, function(error, response, body) {
-				if(!error && response.statusCode == 200) {
-					console.log(JSON.parse(body));
-				}
-				else {
-					return error;
-				}
-			});
-	
-		});
-	});
-	
-	
-	
-	// var flights = request('/api/flights/'+fromAirportCode+'/'+toAirportCode+'/'+now+'/'+later, function(error, response, body) {
-	// 	if(!error && response.statusCode == 200) {
-	// 		return body;
-	// 	}
-	// 	else {
-	// 		return error;
-	// 	}
-	// });
-	
-});
-
-
 router.get('/uber/:startLat/:startLon/:endLat/:endLon', function(req, res, next){
 	var startLat = req.params.startLat;
 	var startLon = req.params.startLon;
