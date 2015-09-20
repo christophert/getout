562e8e1f4f968a4f5f6ed6ad475765eaabca534f
officer <me@christran.in>
2015-09-20 05:14:12 -0400

    jfsdkfjkdsf

--- public/js/apquery.js
+++ public/js/apquery.js
@@ -14,7 +14,7 @@ var processingAJAX = $.ajax({
 });
 	
 $("#tag_holder div:nth-of-type(1)").ready(function() {
-	console.log(processingAJAX);
-	var fltsegment = processingAJAX.responseJSON.flights[0]['tripSegment'][0];
+	var resp = processingAJAX;
+	var fltsegment = resp.responseJSON.flights[0]['tripSegment'][0];
 	$("p#flightId").html(fltsegment[0].airline + " " + fltsegment[0].fltno);
 });
