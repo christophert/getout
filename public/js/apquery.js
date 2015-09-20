252ac9f580b6c575056126a0002180094111f22a
officer <me@christran.in>
2015-09-20 04:30:58 -0400

    update js:

--- public/js/apquery.js
+++ public/js/apquery.js
@@ -4,7 +4,12 @@
 $("#tag_holder div:nth-of-type(1)").ready(function() {
 	$.ajax({
 		type: "GET",
-		url: "/api/flights/"
-	})
-	$("p#flightId").html()
+		url: "http://aggregate.gogogogo.co/yolo/yolo/5/rochester/pittsburgh",
+		timeout: 2000,
+		success: function(r) {
+			console.log(r);
+			// $("p#flightId").html()
+		}
+	});
+	// $("p#flightId").html()
 }
