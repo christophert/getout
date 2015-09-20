c9bb962ea60537986325466a7d5a7e260c2f1083
officer <me@christran.in>
2015-09-20 05:20:55 -0400

    asasds

--- public/js/apquery.js
+++ public/js/apquery.js
@@ -9,8 +9,8 @@ $("#tag_holder div:nth-of-type(1)").ready(function() {
 		url: "http://aggregate.gogogogo.co/yolo/yolo/5/rochester/pittsburgh",
 		timeout: 10000,
 		success: function(r) {
-			console.log(r.flights);
-			$.each(r.flights, function(i, flight) {
+			console.log(r.flights[0]);
+			$.each(r.flights[0], function(i, flight) {
 				console.log(flight);
 			});
 		},
