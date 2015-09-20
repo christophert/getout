7635065ed1619c46a556d8f4bb04db6e4b217188
officer <me@christran.in>
2015-09-20 05:43:52 -0400

    asdf

--- public/js/apquery.js
+++ public/js/apquery.js
@@ -3,23 +3,34 @@
 
 var processingAJAX = 
 	
-$("#tag_holder div:nth-of-type(1)").ready(function() {
+
+$.ajax({
+	type: "GET",
+	url: "http://aggregate.gogogogo.co/yolo/yolo/5/rochester/pittsburgh",
+	timeout: 10000,
+	success: function(r) {
+		console.log(r);
+		var tripsegment = r.flights[0].tripSegment;
+		$("#tag_holder div:nth-of-type(1)").ready(function() {
+			$("p#flightId").html(tripsegment[0][0].flightID);
+			$("p#airline").html(tripsegment[0][0].airline);
+			$("p#fltno").html(tripsegment[0][0].fltno);
+			$("p#depart").html(tripsegment[0][0].depart);
+			$("p#arrival").html(tripsegment[1][0].arrival);
+			
+		});
+		$("#tag_holder div:nth-of-type(2)").ready(function() {
+			// $("p#name")
+		});
+	},
+	error: function(xhr, textStatus, errorThrown) {
+		return textStatus;
+	}
+});
+
+
 	$.ajax({
 		type: "GET",
-		url: "http://aggregate.gogogogo.co/yolo/yolo/5/rochester/pittsburgh",
-		timeout: 10000,
-		success: function(r) {
-			console.log(r.flights[0]);
-			$.each(r.flights[0].tripSegment, function(i, flight) {
-				console.log(flight);
-				$("p#flightId").html(flight[0].flightID);
-				$("p#airline").html(flight[0].airline);
-				$("p#fltno").html(flight[0].fltno);
-				
-			});
-		},
-		error: function(xhr, textStatus, errorThrown) {
-			return textStatus;
-		}
-	});
-});
+		url: "http://aggregate.gogogogo.co/yolo/yolo/"
+	})
+})
