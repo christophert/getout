f8c8be68dc5fcefe8de8adfba5ad2d9b6d97d789
starman360 <anmol.modur@gmail.com>
2015-09-20 07:18:28 -0400

    here

--- public/js/jq.js
+++ public/js/jq.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function() {
     var location = document.getElementsByName('input')[0].value;
     var numOfDays = document.getElementsByName('input')[1].value;
-    $("#trigger").click().attr("onclick","location.herf='/plan?days=' +  numOfDays + '&to=' + location;");
+    $("#trigger").click().attr("onclick","location.herf='/plan';");
 });
